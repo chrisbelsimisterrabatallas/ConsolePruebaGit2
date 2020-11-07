c1bf3d07e278fa871ccbc61c31b80176f1831307
CHRISBEL SIMISTERRA <chrisbel.simisterra@pucese.edu.ec>
2020-11-07 02:27:16 -0500

    Agregar archivos de proyecto.

---
+++ ConsolePruebaGit2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePruebaGit2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo C#");
+            Console.WriteLine("Integracion de VS con Git");
+            Console.ReadKey();
+        }
+    }
+}
